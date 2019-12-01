f44fcea753427dfa4a2acaf6ac10e512cc86133c
Danny Byrne <dannybyrne@DANIELs-MacBook-Pro.local>
2019-11-30 16:24:58 -0800

    added useRef

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,50 +1,56 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
-    useEffect(() => {
-        console.log('cockpit.js in useEffect')
-        const timer = setTimeout(()=> {
-            // alert('saved data to cloud')
-        }, 1000);
-        //function to be ran after the cycle.  
-        return () => {
-            clearTimeout(timer);
-            console.log('cockpit.js cleanup work in useEffect')
-        }
-        //pass in empty array as second argument to have function run when component is removed
-    }, []);
-    //or pass in array with certain data to watch, and only run when that data is updated
+  const toggleBtnRef = useRef(null);
 
-    useEffect(()=> {
-        console.log('cockpit.js cleanup work in useEffect')
-        return () => {
-            console.log('Cockpit.js cleanup work in useEfect2')
-        }
-    })
 
-    const assignedClasses = [];
-    let btnClass = '';
-    if(props.showPersons){
-        btnClass = classes.Red;
+  useEffect(() => {
+    console.log('cockpit.js in useEffect')
+    // const timer = setTimeout(() => {
+    //   // alert('saved data to cloud')
+    // }, 1000);
+    //function to be ran after the cycle.  
+    toggleBtnRef.current.click()
+    return () => {
+      console.log('cockpit.js cleanup work in useEffect')
     }
+    //pass in empty array as second argument to have function run when component is removed
+  }, []);
+  //or pass in array with certain data to watch, and only run when that data is updated
 
-    if (props.personsLength <= 2) {
-      assignedClasses.push(classes.red); // classes = ['red']
+  useEffect(() => {
+    console.log('cockpit.js cleanup work in useEffect')
+    return () => {
+      console.log('Cockpit.js cleanup work in useEfect2')
     }
-    if (props.personsLength <= 1) {
-      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
-    }
+  })
 
-    return (
-        <div className = {classes.Cockpit}>
-            <h1>{props.title}</h1>
-            <p className={assignedClasses.join(' ')}>This is really working!</p>
-            <button className={btnClass} onClick={props.clicked}>
-            Toggle Persons
-            </button>
-        </div>
-    );
+  const assignedClasses = [];
+  let btnClass = '';
+  if (props.showPersons) {
+    btnClass = classes.Red;
+  }
+
+  if (props.personsLength <= 2) {
+    assignedClasses.push(classes.red); // classes = ['red']
+  }
+  if (props.personsLength <= 1) {
+    assignedClasses.push(classes.bold); // classes = ['red', 'bold']
+  }
+
+  return (
+    <div className={classes.Cockpit}>
+      <h1>{props.title}</h1>
+      <p className={assignedClasses.join(' ')}>This is really working!</p>
+      <button
+        ref={toggleBtnRef}
+        className={btnClass}
+        onClick={props.clicked}>
+        Toggle Persons
+      </button>
+    </div>
+  );
 };
 
 export default React.memo(cockpit);
--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
-
+import PropTypes from'prop-types';
 import withClass from '../../../hoc/withClass';
 import classes from './Person.css';
 
 class Person extends Component {
+  constructor(props) {
+    super(props)
+    this.inputElementRef = React.createRef();
+  }
+
+  componentDidMount() {
+    // this.inputElement.focus();
+    this.inputElementRef.current.focus();
+  }
+
   render() {
     console.log('person.js rendering')
     return (
@@ -12,11 +22,23 @@ class Person extends Component {
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
         <p>{this.props.children}</p>
-        <input  type="text" onChange={this.props.changed} value={this.props.name} 
-      />
-    </React.Fragment>
+        <input 
+            // ref={(inputEl) => {this.inputElement = inputEl}}
+            ref={this.inputElementRef}
+            type="text" 
+            onChange={this.props.changed} 
+            value={this.props.name} 
+        />
+      </React.Fragment>
     );
   }
+};
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
 };
 
 export default withClass(Person, classes.Person);
